56d996a4e3d54d17cb8eddbda75ec10952ad11cf
gdorta33 <129328531+gdorta33@users.noreply.github.com>
2023-04-12 09:49:26 -0300

    Latest Version - With guide and Change of logic

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-var selectedDays = [];
-var unselectedDays = [];
-var chosenWeekDayArray = [];
 var currentMonthDays = [];
 var daysToWork = [];
-var leaveDays = [] //Holidays, sick leave, study leave, etc...
+var daysToWorkArray = [];
 
 const date = new Date();
 
@@ -18,17 +15,17 @@ const renderCalendar = () => {
   const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
   const nextDays = 6 - lastDayIndex;
   const months = [
-    "January",  
-    "February",  
-    "March",  
-    "April",  
-    "May",  
-    "June",  
-    "July",  
-    "August",  
-    "September",  
-    "October",  
-    "November",  
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
     "December",
   ]
 
@@ -36,15 +33,14 @@ const renderCalendar = () => {
 
   document.querySelector(".date p").innerHTML = new Date().toDateString();
 
-  //Reset unselectedDays
-  unselectedDays.forEach(x=>unselectedDays.splice(unselectedDays.indexOf(x)));
-
   if(new Date().getMonth() != date.getMonth()){
     document.querySelector(".date p").className = "otherMonth";
   } 
 
 
   let days = "";
+
+  /* ====== Start Print ====== */
 
   // Add the last days of the last month
   for(let x = firstDayIndex; x > 0 ;x--) {
@@ -54,19 +50,11 @@ const renderCalendar = () => {
 
   //Add the days of the current months
   for(let i = 1; i <= lastDay ;i++) {
-    if(selectedDays.includes(i)){
-      days += `<div class="day-select">${i}</div>`;
-    } 
-    else if(leaveDays.includes(i)){
-      days += `<div class="leave-day">${i}</div>`;
-    }
-    else if(i === (new Date().getDate()) && (date.getMonth() === new Date().getMonth())){
+    if(i === (new Date().getDate()) && (date.getMonth() === new Date().getMonth())){
       days += `<div class="today">${i}</div>`;
-      unselectedDays.push(i)
     }
-    else {
+    else{
       days += `<div>${i}</div>`;
-      unselectedDays.push(i)
     }
   }
 
@@ -76,13 +64,15 @@ const renderCalendar = () => {
   }
 
 
-  //Print the days.
+  //Print
   monthDays.innerHTML = days;
-  
-  //WeekEnd
+
+  /* ====== End Print ====== */
+
+  //WeekEnd (It is w-1 in the array because the for, start in 1 and the array start in 0)
   currentMonthDays = document.querySelectorAll('.days div:not([class="prev-month"], [class="next-month"])');
   for(let w = 0; w < currentMonthDays.length; w++){
-    let weekEnd = new Date(date.getFullYear(), date.getMonth(), w+1).getDay();
+    let weekEnd = new Date(date.getFullYear(), date.getMonth(), w + 1).getDay();
     if((weekEnd == 0 || weekEnd == 6) && (currentMonthDays[w].className == '')){
       currentMonthDays[w].className += 'no-work';
     }
@@ -92,48 +82,57 @@ const renderCalendar = () => {
   }
 
   //Add the day-select class and event listener in each day
-  daysToWork = document.querySelectorAll('.days div:not([class="prev-month"], [class="next-month"], [class*="no-work"]');
+  daysToWork = document.querySelectorAll('.days div:not([class="prev-month"], [class="next-month"], [class*="no-work"])');
     
   for(let y = 0; y < daysToWork.length; y++){
     let day = parseInt(daysToWork[y].innerText);  
     daysToWork[y].addEventListener("click", () => {
       if (daysToWork[y].className == ""){
-        selectedDays.push(day);
-        selectedDays = selectedDays.filter((item, pos) => selectedDays.indexOf(item) == pos);
-      }
-      else if(daysToWork[y].className == "day-select"){
-        leaveDays.push(day);
-        selectedDays.splice(selectedDays.indexOf(day),1);
-      }
-      else if(daysToWork[y].className == "leave-day"){
-        leaveDays.splice(leaveDays.indexOf(day),1);
-      }
-      //.today Cases
+        daysToWork[y].className = "day-select"
+      } 
       else if(daysToWork[y].className == "today"){
-        selectedDays.push(day);
+        daysToWork[y].className += " day-select"
       }
       else if(daysToWork[y].className.includes("day-select")){ 
-        leaveDays.push(day);
-        selectedDays.splice(selectedDays.indexOf(day),1);
+        daysToWork[y].className = daysToWork[y].className.replace('day-select', 'leave-day')
       }
       else if(daysToWork[y].className.includes("leave-day")){ 
-        selectedDays.splice(selectedDays.indexOf(day),1);
+        daysToWork[y].className = daysToWork[y].className.replace('leave-day', '').trim()
       }
-      renderCalendar();
     });
   }
     
-  //Delete the repeated days
-  unselectedDays = unselectedDays.filter((item, pos) => unselectedDays.indexOf(item) == pos);
-  leaveDays = leaveDays.filter((item, pos) => leaveDays.indexOf(item) == pos);
+  
+  daysToWorkArray = Array.from(daysToWork);
 }
 
+//Select all the days of z weekDay.
+const weekDay = document.querySelectorAll('.weekdays div');
+for(let z = 0; z < weekDay.length; z++){
+    weekDay[z].addEventListener("click", () => {
+      if((z == 0) || (z == 6)){
+        weekDay[z].className = 'weekend';
+      }
+      else if(weekDay[z].className == '') {
+        weekDay[z].className = 'weekDay-select';
+        //Gives you the days to work in the weekDay column
+        daysToWorkArray.filter((element) => z == new Date(date.getFullYear(), date.getMonth(), element.innerText).getDay()).forEach(x=>x.className.includes("today") ? x.className += " day-select" : x.className = "day-select")
+      } 
+      else if (weekDay[z].className == 'weekDay-select') {
+        weekDay[z].className = 'weekDay-leave';
+        daysToWorkArray.filter((element) => z == new Date(date.getFullYear(), date.getMonth(), element.innerText).getDay()).forEach(x=>x.className.includes("today") ? x.className = x.className.replace('day-select', 'leave-day').trim() : x.className = "leave-day")
+      } 
+      else if (weekDay[z].className == "weekDay-leave") {
+        weekDay[z].className = "";
+        daysToWorkArray.filter((element) => z == new Date(date.getFullYear(), date.getMonth(), element.innerText).getDay()).forEach(x=>x.className.includes("today") ? x.className = x.className.replace("leave-day", "").trim() : x.className="")
+      } 
+  })
+}
 
-//date = newDate()... so wtf with this function. Ohhh the month could be change ater... xd
 const ifMonthEqual = () => { 
   if(new Date().getMonth() === date.getMonth()){
-  document.querySelector(".date p").className = ""
-} 
+    document.querySelector(".date p").className = ""
+  } 
 }
 
 //The < icon to change month
@@ -141,11 +140,8 @@ document.querySelector('.prev').
 addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
   ifMonthEqual();
-  chosenWeekDayArray = [];
-  selectedDays = [];
-  leaveDays.forEach(x=>leaveDays.splice(leaveDays.indexOf(x)));
   document.querySelectorAll('.weekDay-select').forEach(x=>x.className='')
-  document.querySelectorAll('.weekDay-leave').forEach(x=>x.className="")
+  document.querySelectorAll('.weekDay-leave').forEach(x=>x.className='')
   renderCalendar();
 })
 
@@ -154,11 +150,8 @@ document.querySelector('.next').
 addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   ifMonthEqual();
-  chosenWeekDayArray = [];
-  selectedDays = [];
-  leaveDays.forEach(x=>leaveDays.splice(leaveDays.indexOf(x)));
   document.querySelectorAll('.weekDay-select').forEach(x=>x.className='')
-  document.querySelectorAll('.weekDay-leave').forEach(x=>x.className="")
+  document.querySelectorAll('.weekDay-leave').forEach(x=>x.className='')
   renderCalendar();
 })
 
@@ -167,43 +160,13 @@ document.querySelector(".date p").addEventListener("click", () => {
   if(document.querySelector(".date p").className === "otherMonth"){
     date.setMonth(new Date().getMonth());
     document.querySelector(".date p").className = ""
-    chosenWeekDayArray = [];
-    selectedDays = [];
-    leaveDays.forEach(x=>leaveDays.splice(leaveDays.indexOf(x)));
     document.querySelectorAll('.weekDay-select').forEach(x=>x.className='')
-    document.querySelectorAll('.weekDay-leave').forEach(x=>x.className="")
+    document.querySelectorAll('.weekDay-leave').forEach(x=>x.className='')
     renderCalendar();
   }
 })
 
-//Select all the days of z weekDay.
-const weekDay = document.querySelectorAll('.weekdays div');
 
-for(let z = 0; z < weekDay.length; z++){
-    weekDay[z].addEventListener("click", () => {
-      if((z == 0) || (z == 6)){
-        weekDay[z].className = 'weekend';
-      }
-      else if(weekDay[z].className == '') {
-        weekDay[z].className = 'weekDay-select';
-        //Add all the selected days of z weekdays
-        selectedDays = selectedDays.concat(unselectedDays.filter((element) => z == new Date(date.getFullYear(), date.getMonth(), element).getDay()),
-        leaveDays.filter((element) => z == new Date(date.getFullYear(), date.getMonth(), element).getDay()));
-      } 
-      else if (weekDay[z].className == 'weekDay-select') {
-        weekDay[z].className = 'weekDay-leave';
-        //Add the leave days
-        leaveDays = leaveDays.concat(selectedDays.filter((element) => z == new Date(date.getFullYear(), date.getMonth(), element).getDay()));
-        //Delete the selected days
-        selectedDays = selectedDays.filter(element => z !== new Date(date.getFullYear(), date.getMonth(), element).getDay())
-      } 
-      else if (weekDay[z].className == 'weekDay-leave') {
-        weekDay[z].className = '';
-        leaveDays = leaveDays.filter(element => z !== new Date(date.getFullYear(), date.getMonth(), element).getDay())
-      } 
-    renderCalendar();
-  })
-}
 
 renderCalendar();
 
@@ -225,3 +188,21 @@ function firstMessage(){
 }
 
 document.getElementById("counter").addEventListener("click", counterDays)
+
+const arrow = document.getElementById('arrow');
+
+arrow.addEventListener("click",function(){
+  if(arrow.innerText=='>'){
+  document.querySelector('.guide-container').style.left='0rem';
+  arrow.style.left="0rem"
+  arrow.innerText='<'
+  }
+  else{
+    document.querySelector('.guide-container').style.left='-35rem';
+    arrow.style.left="-35rem"
+    arrow.innerText='>'
+  }
+})
+
+
+
